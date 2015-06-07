6fef951d112c94973ab88abe12ba506dab02f78d
Tammy Peters <tamarapeters2@gmail.com>
2015-06-07 15:59:40 -0400

    view and controller updates

--- app/controllers/butterflies_controller.rb
+++ app/controllers/butterflies_controller.rb
@@ -7,4 +7,21 @@ class ButterfliesController < ApplicationController
     @butterfly = Butterfly.find(params[:id])
   end
 
+  def new
+    @butterfly = Butterfly.new
+  end
+
+  def create
+    @butterfly = Butterfly.new(butterfly_params)
+      if @butterfly.save
+        redirect_to 'butterflies'
+      else
+        render 'new'
+      end
+
+  private
+    def butterfly_params
+      params.require(:butterfly).permit(:name)
+    end
+
 end
