25f81b589d10296f4b2433aa67f2a676ab7c27ce
Tammy Peters <tamarapeters2@gmail.com>
2015-06-06 23:17:40 -0400

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,16 @@ User.destroy_all
 
 stages = [
   {
-    name: "Honeymoon Stage",
-    description: "This is a loving, non-violent time, often where the abuser tries to compensate for previous mistreatment of the other"
+    name:        "Honeymoon Stage",
+    description: "This is a loving, non-violent time, often where the abuser tries to compensate for previous mistreatment of the other.  Unless there is intervention where the abuser seriously commits to counseling to discover underlying issues and learn to manage his stress, the dangerous tension will begin to build again."
   },
   {
-    name: "Tension-Building Stage",
-    description: "The demands on and stresses of the relationship increase"
+    name:        "Tension-Building Stage",
+    description: "The demands on and stresses of the relationship increase.  This is the longest stage, which can last from days to years.  Like the explosive stage, it does not occur until after the people involved have become extremely close for some time.  Over time, the abuser becomes more stressed and more controlling of the victim."
   },
   {
-    name: "Explosive Stage",
-    description: "Seeking a release from the build-up of stress and tension, the abuser makes a choice about how to strike out and does it"
+    name:        "Explosive Stage",
+    description: "Seeking a release from the build-up of stress and tension, the abuser instigates an incident, striking out against the victim -  verbally, emotionally, or physically. This is the shortest stage, but often increases in severity each time through the cycle."
   },
 
 ]
@@ -31,11 +31,11 @@ end
 
 roles = [
   {
-    name: "Abuser",
+    name:        "Abuser",
     description: "Generally, one who tries to compensate for feeling like he or she is powerless by asserting unhealthy control over someone close"
   },
   {
-    name: "Victim",
+    name:        "Victim",
     description: "One who is trapped physically, financially, or emotionally by someone close and often suffers in mind, body, or spirit"
   },
 ]
@@ -99,12 +99,181 @@ behaviors = [
   },
 
   {
-    role:        Role.find_by(name: "Abuser"),
+    role:        Role.find_by(name: "Victim"),
     stage:       Stage.find_by(name:"Explosive Stage"),
     description: "May stimulate confrontation to get the 'event' over with"
   },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "Often deny or minimize the seriousness of the injuries to the abuser to soothe them or avoid further abuse"
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "Must wait it out or try to escape.  Fighting back or retaliating will simply increase the abuser's will to attack more aggressively."
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "Denies his or her own insecurities or frustrations"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "Blames victim or other for making him or her miserable"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "Becomes increasingly aggressive towards objects and verbally abusive towards the victim"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "Becomes increasingly demanding of control over the victim's life, including dictating who he or she may spend time with or how he or she may dress"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "Alters behavior to avoid triggering the abuser's aggression"
+  },
+
 ]
+
 
 behaviors.each do |behaviors_hash|
   Behavior.create!(behaviors_hash)
+end
+
+attitudes = [
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "out of control"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "that the victim is to blame"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "angry"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "the desire to control or punish the victim"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "calm, immediately after the event, because the pent-up stress has been eliminated.
+                  Abusers often appear peaceful by the time law enforcement might arrive."
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "terrified"
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "shocked"
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "ashamed"
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "the desire to get the pain over with"
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Explosive Stage"),
+    description: "the desire to get the pain over with"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "powerless in own home"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "frustrated, disappointed, or insecure with things in life"
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "like he or she must walk on eggshells, being very careful not to trigger an explosion"
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "that nothing he or she does is good enough for the abuser"
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "angry, because the abuser is not keeping the promises made during the honeymoon stage"
+  },
+
+  {
+    role:        Role.find_by(name: "Victim"),
+    stage:       Stage.find_by(name:"Tension-Building Stage"),
+    description: "powerless to stop the next beating or explosion"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Honeymoon Stage"),
+    description: "temporarily in control of self"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Honeymoon Stage"),
+    description: "fearful that the victim will leave"
+  },
+
+  {
+    role:        Role.find_by(name: "Abuser"),
+    stage:       Stage.find_by(name:"Honeymoon Stage"),
+    description: "shame or guilt over his or her behavior"
+  },
+
+
+
+]
+
+
+attitudes.each do |attitudes_hash|
+  Attitude.create!(attitudes_hash)
 end
