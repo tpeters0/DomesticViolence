9a6b1d414949dbfd76c5a3c4b55183f879266419
Tammy Peters <tamarapeters2@gmail.com>
2015-06-08 03:01:36 -0400

    experimenting

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -68,4 +68,144 @@ $(document).ready(function(){
 
 
 
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    interact('.draggable')
+      .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+          restriction: "parent",
+          endOnly: true,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
+
+      textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(event.dx * event.dx +
+                     event.dy * event.dy)|0) + 'px');
+    }
+  });
+
+
+    function dragMoveListener (event) {
+      var target = event.target,
+          // keep the dragged position in the data-x/data-y attributes
+          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+      // translate the element
+      target.style.webkitTransform =
+      target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+
+      // update the position attributes
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+
+      console.log(target);
+    }
+
+    // this is used later in the resizing demo
+    window.dragMoveListener = dragMoveListener;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // function draw_1() {
+    //   var canvas1 = document.getElementById("drawing1");
+    //   var context = canvas1.getContext("2d");
+    //
+    //   // Create the yellow face
+    //   context.strokeStyle = "#000000";
+    //   context.fillStyle = "#FFFF00";
+    //   context.beginPath();
+    //   context.arc(100,100,20,0,Math.PI*2,true);
+    //   context.closePath();
+    //   context.stroke();
+    //   context.fill();
+
+
+
+      // canvas1.mousedown(function(e){
+      //   var mouseX = e.pageX - this.offsetLeft;
+      //   var mouseY = e.pageY - this.offsetTop;
+      //
+      //   paint = true;
+      //   addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+      //   console.log("downed!")
+      //   redraw();
+      // });
+      //
+
+
+      // context.strokeStyle = "black";
+      // context.stroke();
+      //
+      // context.beginPath();
+      // context.moveTo(0, 40);
+      // context.lineTo(240, 40);
+      // context.moveTo(260, 40);
+      // context.lineTo(500, 40);
+      // context.moveTo(495, 35);
+      // context.lineTo(500, 40);
+      // context.lineTo(495, 45);
+
+
+
+      // context.fillRect(50, 25, 30, 15);
+    // }
+    //
+    // $("#click").click(function(){
+    //   location.href = "#";
+    //   draw_1();
+    //   return false;
+    //
+    // })
+
+
 });
