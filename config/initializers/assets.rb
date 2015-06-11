c701ed5d671455fe235bc709506bfe355d70c364
Tammy Peters <tamarapeters2@gmail.com>
2015-06-11 09:55:21 -0400

    separate canvas_script.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( paper_script.js )
 
 Rails.application.config.assets.precompile += %w( script.js )
-
+Rails.application.config.assets.precompile += %w( canvas_script.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
