f2d816e5bf05970358ef7f3c1996f24b4b12e831
Tammy Peters <tamarapeters2@gmail.com>
2015-06-20 20:07:25 -0400

    rouge script alt

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,6 @@
 Rails.application.config.assets.version = '1.0'
 
 # Rails.application.config.assets.precompile += %w( paper_script.js )
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 Rails.application.config.assets.precompile += %w( script.js )
 
 
