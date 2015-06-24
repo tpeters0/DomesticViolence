ed13b8e46636c7134d2875d5fcac71ef6409bdde
Tammy Peters <tamarapeters2@gmail.com>
2015-06-24 00:46:27 -0400

    separate show-stages and butterflies stylesheets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,8 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Rails.application.config.assets.precompile += %w( paper_script.js )
 Rails.application.config.assets.precompile += %w( script.js )
-
-
+Rails.application.config.assets.precompile += %w( show-stages.css )
+Rails.application.config.assets.precompile += %w( butterflies.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
